772d933e702377c0aed46b4ecd179d80a8676b38
Seongyong Park <EuphoricCatface@gmail.com>
2022-02-01 11:04:13 +0900

    Fill simple tiles

--- widget/BoardFrame.py
+++ widget/BoardFrame.py
@@ -16,23 +16,14 @@ class BoardWidget(QFrame):
         self.setFrameStyle(QFrame.Sunken | QFrame.StyledPanel)
         self.setAcceptDrops(True)
 
-        button1 = TileButton.TileButton(self)
-        button1.setText('a')
-        button1.move(10, 10)
-        button1.show()
-        button1.setAttribute(Qt.WA_DeleteOnClose)
-
-        button2 = TileButton.TileButton(self)
-        button2.setText('b')
-        button2.move(100, 10)
-        button2.show()
-        button2.setAttribute(Qt.WA_DeleteOnClose)
-
-        button3 = TileButton.TileButton(self)
-        button3.setText('c')
-        button3.move(10, 80)
-        button3.show()
-        button3.setAttribute(Qt.WA_DeleteOnClose)
+        button_columns: [list[list[TileButton.TileButton]]] = [
+            [TileButton.TileButton(self) for _ in range(5)]
+            for _ in range(5)
+        ]
+        for x, column in enumerate(button_columns):
+            for y, e in enumerate(column):
+                e.move(x * 40, 200 - (y + 1) * 40)
+                e.setFixedSize(40, 40)
 
     def start_drag(self, pos: QPoint) -> None:
         print(pos)
