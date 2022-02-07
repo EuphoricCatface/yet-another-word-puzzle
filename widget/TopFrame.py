897a708222e6b4ec6fbcaf35f4877d30fa196661
Seongyong Park <EuphoricCatface@gmail.com>
2022-02-07 22:21:34 +0900

    Add score in TopFrame

--- widget/TopFrame.py
+++ widget/TopFrame.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QFrame
+from PySide6.QtCore import Slot
 from widget.ui_TopFrame import Ui_Frame
 
 
@@ -7,3 +8,11 @@ class TopFrame(QFrame):
         super(TopFrame, self).__init__(parent)
         self.ui = Ui_Frame()
         self.ui.setupUi(self)
+
+        self.score = 0
+        self.ui.label_score.setText(str(self.score))
+
+    @Slot(int)
+    def add_score(self, score: int):
+        self.score += score
+        self.ui.label_score.setText(str(self.score))
