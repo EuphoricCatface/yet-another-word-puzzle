b65e15c222d87c424728c6f72fc0059cf3e4d5d2
Seongyong Park <EuphoricCatface@gmail.com>
2022-02-19 17:16:42 +0900

    Create Set/Get seed dialog

--- widget/MainWindow.py
+++ widget/MainWindow.py
@@ -1,9 +1,40 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QDialog
+from PySide6.QtWidgets import QVBoxLayout, QLabel, QLineEdit, QCheckBox
 from PySide6.QtCore import QObject, QTimer
 
 from widget.ui_MainWindow import Ui_MainWindow
 
 from time import monotonic
+
+
+class GetSetRandDialog(QDialog):
+    def __init__(self, parent):
+        super(GetSetRandDialog, self).__init__(parent)
+        self.setWindowTitle("Seeds")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.get_seed_label = QLabel()
+        self.get_seed_label.setText("Current seed:")
+        self.layout.addWidget(self.get_seed_label)
+        self.get_seed_lineedit = QLineEdit()
+        self.get_seed_lineedit.setReadOnly(True)
+        self.layout.addWidget(self.get_seed_lineedit)
+
+        self.set_seed_label = QLabel()
+        self.set_seed_label.setText("Next seed:")
+        self.layout.addWidget(self.set_seed_label)
+        self.set_seed_lineedit = QLineEdit()
+        self.layout.addWidget(self.set_seed_lineedit)
+
+        self.confirm_check = QCheckBox()
+        self.confirm_check.setText("Apply")
+        self.layout.addWidget(self.confirm_check)
+        self.persist_check = QCheckBox()
+        self.persist_check.setText("Repeat seed")
+        self.persist_check.setEnabled(False)
+        self.layout.addWidget(self.persist_check)
+        self.confirm_check.stateChanged.connect(self.persist_check.setEnabled)
 
 
 class MainWindow(QMainWindow):
@@ -37,6 +68,9 @@ class MainWindow(QMainWindow):
         # TODO: De-/activate undo/redo on condition
         self.ui.actionUndo.triggered.connect(self.ui.frame_Board.undo)
         self.ui.actionRedo.triggered.connect(self.ui.frame_Board.redo)
+
+        self.seed_dialog = GetSetRandDialog(self)
+        self.ui.actionSet_Get_seed.triggered.connect(self.seed_dialog.show)
 
         self.adjustSize()
 
